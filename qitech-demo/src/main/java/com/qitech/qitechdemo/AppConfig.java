7a082a1081d012feb624dd36f9ee5e23cffc6835
xinbaojian <xinbaojian@126.com>
2019-12-13 15:17:08 +0800

    qitech-demo 集成redis

--- qitech-demo/src/main/java/com/qitech/qitechdemo/AppConfig.java
+++ qitech-demo/src/main/java/com/qitech/qitechdemo/AppConfig.java
@@ -1,12 +1,15 @@
 package com.qitech.qitechdemo;
 
+import com.qitech.qitechdemo.config.RedisConfig;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author xinbj
  */
 @Configuration
 @EnableAutoConfiguration
+@Import({RedisConfig.class})
 public class AppConfig {
 }
--- qitech-demo/src/main/java/com/qitech/qitechdemo/service/impl/DictServiceImpl.java
+++ qitech-demo/src/main/java/com/qitech/qitechdemo/service/impl/DictServiceImpl.java
@@ -1,10 +1,12 @@
 package com.qitech.qitechdemo.service.impl;
 
+import com.qitech.qitechdemo.annotation.CacheExpire;
 import com.qitech.qitechdemo.dao.SysDictMapper;
 import com.qitech.qitechdemo.entity.SysDict;
 import com.qitech.qitechdemo.entity.SysDictExample;
 import com.qitech.qitechdemo.service.IDictService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,6 +23,8 @@ public class DictServiceImpl implements IDictService {
     @Autowired
     private SysDictMapper sysDictMapper;
 
+    @CacheExpire(500)
+    @Cacheable(value = "dict:list", key = "#root.methodName")
     @Override
     public List<SysDict> list() {
         return sysDictMapper.selectByExample(new SysDictExample());
