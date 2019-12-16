b4d6c2bc8e16ccc395e33a5516d3c4d6de0aaddd
xinbaojian <xinbaojian@126.com>
2019-12-16 17:50:51 +0800

    init rbac module

---
+++ qitech-rbac/src/main/java/com/qitech/qitechrbac/QitechRbacApplication.java
@@ -0,0 +1,13 @@
+package com.qitech.qitechrbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QitechRbacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QitechRbacApplication.class, args);
+    }
+
+}
